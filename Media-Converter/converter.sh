013727b183c4c7857fc235e762d898523e3485a9
DESKTOP-4NP9D3F\Tintin <tristankuisis1@gmail.com>
2019-08-04 14:51:50 +0200

    Updated conversion script. Still only does one folder.

--- Media-Converter/converter.sh
+++ Media-Converter/converter.sh
@@ -4,15 +4,19 @@ cd ~
 cd D:/
 ls
 
-# This will list the files in the folder where there are avi files present.
+# The script will need to replace spaces in each of the file and folder names such that the script doesn't break.
+find . -depth -name '* *' \
+| while IFS=' ' read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
+
+# List the items that will be converted
 find Movies2/ -maxdepth 4 -name "*.avi"  -execdir ls \;
 
-find Movies2/ -maxdepth 4 -name '*.avi' -exec bash -c 'ffmpeg -i $0 -vcodec copy -acodec copy  ${0/avi/mkv}' {} \;
+# Convert the files. This appears to convert the system within the avi and mkv containers.
+# find Movies2/ -maxdepth 4 -name '*.avi' -exec bash -c 'ffmpeg -i $0 ${0/avi/mkv}' {} \;
 
+# This will simply copy the video and audio bitstream without quality loss. It simply changes the container.
+find Movies2/ -maxdepth 4 -name '*.avi' -exec -c 'ffmpeg -i $0 -c:v copy -c:a copy ${0/avi/mkv}' {} \;
 
+# Delete the old file.
+find Movies2/ -maxdepth 4 -f '*.avi' -exec rm -f {} \;
 
-# for i in *.avi;
-#     do name=`echo "$i" | cut -d'.' -f1`
-#     echo "$name"
-# #     # ffmpeg -i "$i" "${name}.mp4"
-# done

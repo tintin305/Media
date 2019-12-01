16f50691ce5023843386aa3c9118a4115c604401
Tristan Kuisis <tristankuisis1@gmail.com>
2019-12-01 15:12:07 +0200

    Cleaned up extract script.

--- Media-Backup/extract.py
+++ Media-Backup/extract.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-movies = 'Movies-Level 2-20190702-092222.csv'
-movieData = pd.read_csv(movies)
+movieFileName = 'Moviescsv'
+movieData = pd.read_csv(movieFileName)
 
 movieTitles = movieData['Title']
 
 movieTitles.to_csv('movieTitles.csv', index=False)
 
-shows = 'TV Shows-Level 2-20190702-092627.csv'
-showData = pd.read_csv(shows)
+showFileName = 'TVShows.csv'
+showData = pd.read_csv(showFileName)
 
 showTitles = showData['Series Title'].drop_duplicates()
 
